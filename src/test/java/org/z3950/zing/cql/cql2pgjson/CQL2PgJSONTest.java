1c6923165fa161672f0deffa56b51f6fa6fc9b18
Julian Ladisch <julianladisch@users.noreply.github.com>
2017-03-31 18:21:59 +0200

    FOLIO-518 allow encoding by creating database using template0

--- src/test/java/org/z3950/zing/cql/cql2pgjson/CQL2PgJSONTest.java
+++ src/test/java/org/z3950/zing/cql/cql2pgjson/CQL2PgJSONTest.java
@@ -76,7 +76,7 @@ public class CQL2PgJSONTest {
         if ("postgres".equals(conn.getCatalog())) {
           try (Statement stmt = conn.createStatement()) {
             stmt.executeUpdate("DROP DATABASE IF EXISTS " + dbName);
-            stmt.executeUpdate("CREATE DATABASE " + dbName + " ENCODING='UTF8' LC_COLLATE='C' LC_CTYPE='C'");
+            stmt.executeUpdate("CREATE DATABASE " + dbName + " TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C' LC_CTYPE='C'");
           }
           catch (SQLException e) {
             System.out.println(e.getMessage());
