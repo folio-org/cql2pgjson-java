991bc5f3bcd39d2d67c7f53a046d30e49bb72fab
Frances Webb <frances.webb@cornell.edu>
2017-07-19 10:18:58 -0400

    Add test cases

--- src/test/java/org/z3950/zing/cql/cql2pgjson/MultiFieldProcessingTest.java
+++ src/test/java/org/z3950/zing/cql/cql2pgjson/MultiFieldProcessingTest.java
@@ -2,7 +2,9 @@ package org.z3950.zing.cql.cql2pgjson;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 import org.junit.Test;
 
@@ -30,6 +32,64 @@ public class MultiFieldProcessingTest {
     assertEquals(
         "field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])[Vv]($|[[:punct:]]|[[:space:]])'",
         converter.cql2pgJson("name=/respectAccents v"));
+  }
+
+  @Test
+  public void testApplicationOfFieldNamesWithSchema()
+      throws FieldException, QueryValidationException, SchemaException, IOException {
+    LinkedHashMap<String,String> fieldsAndSchemas = new LinkedHashMap<>();
+    fieldsAndSchemas.put("field1", Util.getResource("userdata.json"));
+    fieldsAndSchemas.put("field2", Util.getResource("userdata.json"));
+    CQL2PgJSON converter = new CQL2PgJSON( fieldsAndSchemas );
+    assertEquals(
+        "field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])[VvᵛᵥṼṽṾṿⅤⅴⓋⓥⱽＶｖ]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("field1.name=v"));
+    assertEquals(
+        "field2->>'name' ~ '(^|[[:punct:]]|[[:space:]])[vᵛᵥṽṿⅴⓥｖ]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("field2.name=/respectCase v"));
+    assertEquals(
+        "field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])[Vv]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("name=/respectAccents v"));
+  }
+
+  @Test
+  public void testApplicationOfFieldNamesWithServerChoiceIndexes()
+      throws FieldException, QueryValidationException, ServerChoiceIndexesException {
+    CQL2PgJSON converter = new CQL2PgJSON( Arrays.asList("field1","field2") );
+    converter.setServerChoiceIndexes(Arrays.asList("field1.name"));
+    assertEquals(
+        "field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])[VvᵛᵥṼṽṾṿⅤⅴⓋⓥⱽＶｖ]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("v"));
+    converter.setServerChoiceIndexes(Arrays.asList("field2.name"));
+    assertEquals(
+        "field2->>'name' ~ '(^|[[:punct:]]|[[:space:]])[VvᵛᵥṼṽṾṿⅤⅴⓋⓥⱽＶｖ]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("v"));
+    converter.setServerChoiceIndexes(Arrays.asList("name"));
+    assertEquals(
+        "field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])[VvᵛᵥṼṽṾṿⅤⅴⓋⓥⱽＶｖ]($|[[:punct:]]|[[:space:]])'",
+        converter.cql2pgJson("v"));
+  }
+
+  @Test(expected = QueryValidationException.class)
+  public void testSchemaValidationInMultiFieldQuery()
+      throws FieldException, SchemaException, IOException, QueryValidationException {
+    LinkedHashMap<String,String> fieldsAndSchemas = new LinkedHashMap<>();
+    fieldsAndSchemas.put("field1", Util.getResource("userdata.json"));
+    fieldsAndSchemas.put("field2", Util.getResource("userdata.json"));
+    CQL2PgJSON converter = new CQL2PgJSON( fieldsAndSchemas );
+    converter.cql2pgJson("field2.notInSchema=/respectCase v");
+  }
+
+  @Test
+  public void testMixedFieldQuery() throws FieldException, QueryValidationException {
+    CQL2PgJSON converter = new CQL2PgJSON( Arrays.asList("field1","field2") );
+    String expected = 
+        "(field1->>'name' ~ '(^|[[:punct:]]|[[:space:]])Smith($|[[:punct:]]|[[:space:]])')"
+        + " AND (field1->>'email' ~ '(^|[[:punct:]]|[[:space:]])[Gg][Mm][Aa][Iiı][Ll]\\.[Cc][Oo][Mm]($|[[:punct:]]|[[:space:]])')"
+        + " ORDER BY field2->'name'";
+    assertEquals(expected,
+        converter.cql2pgJson("name =/respectCase/respectAccents Smith"
+        + " AND email =/respectAccents gmail.com sortBy field2.name/sort.ascending").replaceAll("\"", "\\\\\""));
   }
 
 }
