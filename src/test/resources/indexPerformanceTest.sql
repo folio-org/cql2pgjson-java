2a5f958e345bd67adc2990ec7efbb49648502dde
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-01-09 10:24:16 +0100

    CQLPG-33: Performance test for number match

--- src/test/resources/indexPerformanceTest.sql
+++ src/test/resources/indexPerformanceTest.sql
@@ -12,7 +12,7 @@ CREATE TABLE config_data (
    jsonb jsonb NOT NULL);
 INSERT INTO config_data
   SELECT id, jsonb_build_object('id',id, 'module','circulation', 'configName','loans',
-    'code',id2, 'description',description, 'value',value, 'default',true,
+    'code',id2, 'num',(id2%100000)/100000.0, 'description',description, 'value',value, 'default',true,
     'enabled',true)
   FROM (  select gen_random_uuid() AS id,
           generate_series(1, 200004) AS id2,
@@ -23,3 +23,4 @@ UPDATE config_data SET jsonb=jsonb_set(jsonb, '{value}', '"a1b2c3d4e5f6 xxxx"') 
 UPDATE config_data SET jsonb=jsonb_set(jsonb, '{value}', '"036000291452"')      WHERE jsonb->'code'='100001';
 UPDATE config_data SET jsonb=jsonb_set(jsonb, '{value}', '"36000291452"')       WHERE jsonb->'code'='100002';
 ANALYSE config_data;
+
