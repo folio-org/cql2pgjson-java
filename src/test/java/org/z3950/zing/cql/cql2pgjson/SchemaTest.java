a70b425bd5a56837ae1aa497a7da24c68632f31a
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-10-14 02:59:38 +0200

    CQLPG-66: Fix validating of array field names

--- src/test/java/org/z3950/zing/cql/cql2pgjson/SchemaTest.java
+++ src/test/java/org/z3950/zing/cql/cql2pgjson/SchemaTest.java
@@ -39,14 +39,15 @@ public class SchemaTest {
     assertThat(s.mapFieldNameAgainstSchema(field).getPath(), is(field));
   }
 
-  /* Both `type` and `properties` are structural keywords, but can still be used for attributes. */
+  /* `type`, `properties` and `items` are structural keywords, but can still be used for attributes. */
   @Test
   @Parameters({
     "type",
-    "child.pet.type",
+    "parent.type",
     "properties",
     "parent.properties",
-    "child.favoriteColor",
+    "items",
+    "parent.items",
   })
   public void noReservedWordsTest(String field) throws Exception {
     Schema s = new Schema(Util.getResource("complex.json"));
@@ -72,6 +73,37 @@ public class SchemaTest {
   }
 
   @Test
+  @Parameters({
+    "child",                        // array (array is object array)
+    "relatives",                    // array (array is string array)
+    "parent.umbrellas",             // object.array (array is object array)
+    "parent.hats",                  // object.array (array is string array)
+    "parent.address.phoneNumbers",  // object.object.array
+  })
+  public void arrayFieldAsLeaf(String field) throws Exception, IOException {
+    Schema s = new Schema(Util.getResource("complex.json"));
+    assertThat(s.mapFieldNameAndTypeAgainstSchema(field, null).getPath(), is(field));
+    assertThat(s.mapFieldNameAndTypeAgainstSchema(field, "string").getPath(), is(field));
+    thrown.expect(QueryValidationException.class);
+    s.mapFieldNameAndTypeAgainstSchema(field, "integer");
+  }
+
+  @Test
+  @Parameters({
+    "child.items",       // array.arrayitem
+    "child.items.pet",   // array.arrayitem.array
+    "child.name",        // array.string
+    "child.pet",         // array.array
+    "child.pet.name",    // array.array.string
+    "parent.hats.type",  // object.array.string
+  })
+  public void arrayFieldAsNonLeaf(String field) throws Exception, IOException {
+    Schema s = new Schema(Util.getResource("complex.json"));
+    thrown.expect(QueryValidationException.class);
+    s.mapFieldNameAndTypeAgainstSchema(field, null);
+  }
+
+  @Test
   public void invalidFieldWithoutPath() throws QueryValidationException, IOException, SchemaException {
     Schema s = new Schema( Util.getResource("userdata.json") );
     expect(QueryValidationException.class, "Field name", "city", "is not present");
@@ -87,19 +119,19 @@ public class SchemaTest {
 
   @Test
   @Parameters({
-    "id,                                       id",
-    "metadata.id,                              metadata.id",
-    "metadata.name,                            metadata.name",
-    "query.term,                               query.term",
-    "query.boolean.op,                         query.boolean.op",
-    "query.boolean.right.term,                 query.boolean.right.term",
-    "query.boolean.right.boolean.left.term,    query.boolean.right.boolean.left.term",
-    "query.prox.term,                          query.prox.term",
-    "query.prox.prox.prox.term,                query.prox.prox.prox.term"
+    "id",
+    "metadata.id",
+    "metadata.name",
+    "query.term",
+    "query.boolean.op",
+    "query.boolean.right.term",
+    "query.boolean.right.boolean.right.term",
+    "query.boolean.right.boolean.left.term",
+    "query.boolean.left.boolean.right.term",
   })
-  public void testRef(String field, String fullField) throws IOException, SchemaException, QueryValidationException {
+  public void testRef(String field) throws IOException, SchemaException, QueryValidationException {
     Schema s = new Schema(Util.getResource("refs.json"));
-    assertThat(s.mapFieldNameAgainstSchema(field).getPath(), is(fullField));
+    assertThat(s.mapFieldNameAgainstSchema(field).getPath(), is(field));
   }
 
   @Test
