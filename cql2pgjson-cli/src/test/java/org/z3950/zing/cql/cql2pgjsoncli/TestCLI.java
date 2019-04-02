75a1d16002bf01d0d20bab28abfc727e3c15423e
Hongwei Ji <hji@ebsco.com>
2019-04-02 16:31:27 -0400

    CQLPG-90: remove legacy query translation based on individual json schema files

--- cql2pgjson-cli/src/test/java/org/z3950/zing/cql/cql2pgjsoncli/TestCLI.java
+++ cql2pgjson-cli/src/test/java/org/z3950/zing/cql/cql2pgjsoncli/TestCLI.java
@@ -2,7 +2,6 @@ package org.z3950.zing.cql.cql2pgjsoncli;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import org.apache.commons.cli.ParseException;
 import static org.junit.Assert.assertEquals;
@@ -14,8 +13,6 @@ import org.z3950.zing.cql.cql2pgjson.FieldException;
 import org.z3950.zing.cql.cql2pgjson.QueryValidationException;
 import org.z3950.zing.cql.cql2pgjson.SchemaException;
 
-import static org.z3950.zing.cql.cql2pgjsoncli.CQL2PGCLIMain.readFile;
-
 public class TestCLI {
   int exitStatus;
   String instanceSchemaPath;
@@ -26,8 +23,6 @@ public class TestCLI {
   public void setup() throws URISyntaxException {
     exitStatus = 0;
     CQL2PGCLIMain.exit = status -> exitStatus = status;
-    instanceSchemaPath = Paths.get(ClassLoader.getSystemResource("instance.json").toURI()).toString();
-    holdingSchemaPath = Paths.get(ClassLoader.getSystemResource("holdingsrecord.json").toURI()).toString();
     dbSchemaPath = Paths.get(ClassLoader.getSystemResource("dbschema.json").toURI()).toString();
   }
 
@@ -47,10 +42,6 @@ public class TestCLI {
     assertEquals(0, exitStatus);
   }
 
-  private String handleOptions(String arguments) throws Exception {
-    return CQL2PGCLIMain.handleOptions(arguments.split(" "));
-  }
-
   private String handleOptions(String[] arguments) throws Exception {
     return CQL2PGCLIMain.handleOptions(arguments);
   }
@@ -64,19 +55,8 @@ public class TestCLI {
     CQL2PgJSON cql2pgjson = new CQL2PgJSON(fullFieldName);
     String output = CQL2PGCLIMain.parseCQL(cql2pgjson, "instance", cql);
     String cli_output = CQL2PGCLIMain.handleOptions(args);
-    assertNotNull(output);
-    assertEquals(output, cli_output);
-  }
-
-  @Test
-  public void testCLIWithSchema() throws IOException, FieldException, SchemaException,
-      QueryValidationException, ParseException {
-    String cql = "hrid=\"fcd64ce1-6995-48f0-840e-89ffa2\"";
-    String[] args = new String[] {"-t", "instance", "-f", "jsonb", "-s", instanceSchemaPath, cql };
-    String fullFieldName = "instance.jsonb";
-    CQL2PgJSON cql2pgjson = new CQL2PgJSON(fullFieldName, readFile(instanceSchemaPath, Charset.forName("UTF-8")));
-    String output = CQL2PGCLIMain.parseCQL(cql2pgjson, "instance", cql);
-    String cli_output = CQL2PGCLIMain.handleOptions(args);
+    System.out.println(output);
+    System.out.println(cli_output);
     assertNotNull(output);
     assertEquals(output, cli_output);
   }
@@ -87,33 +67,18 @@ public class TestCLI {
     String cql = "hrid=\"fcd64ce1-6995-48f0-840e-89ffa2\"";
     String[] args = new String[] {"-t", "instance", "-f", "jsonb", "-b", dbSchemaPath, cql };
     String fullFieldName = "instance.jsonb";
-    CQL2PgJSON cql2pgjson = new CQL2PgJSON(fullFieldName, null, dbSchemaPath);
+    CQL2PgJSON cql2pgjson = new CQL2PgJSON(fullFieldName);
+    cql2pgjson.setDbSchema(dbSchemaPath);
     String output = CQL2PGCLIMain.parseCQL(cql2pgjson, "instance", cql);
     String cli_output = CQL2PGCLIMain.handleOptions(args);
     assertNotNull(output);
+    System.out.println(output);
+    System.out.println(cli_output);
     assertEquals(output, cli_output);
   }
 
-  @Test
-  public void testCLIFieldSchemaMap() throws Exception {
-    String actualSql = handleOptions("-t item -m src/test/resources/fieldschemamap.json foobar=abc");
-    assertEquals(
-        "select * from item where lower(f_unaccent(item.jsonb->>'foobar')) ~ "
-        + "lower(f_unaccent('(^|[[:punct:]]|[[:space:]]|(?=[[:punct:]]|[[:space:]]))abc($|[[:punct:]]|[[:space:]]|(?<=[[:punct:]]|[[:space:]]))'))",
-        actualSql);
-  }
-
-  @Test
-  public void testCLIFieldSchemaMapString() throws Exception {
-    String actualSql = handleOptions("-t item -m {\"item.jsonb\":\"src/test/resources/item.json\"} foobar=abc");
-    assertEquals(
-        "select * from item where lower(f_unaccent(item.jsonb->>'foobar')) ~ "
-        + "lower(f_unaccent('(^|[[:punct:]]|[[:space:]]|(?=[[:punct:]]|[[:space:]]))abc($|[[:punct:]]|[[:space:]]|(?<=[[:punct:]]|[[:space:]]))'))",
-        actualSql);
-  }
-
   void testCLI(String cql, String expectedSql) throws Exception {
-      String[] args = new String[] { "-t", "instance", "-s", instanceSchemaPath, cql };
+      String[] args = new String[] { "-t", "instance", cql };
       String actualSql = handleOptions(args);
       assertEquals(expectedSql, actualSql);
   }
@@ -136,8 +101,4 @@ public class TestCLI {
         "select * from instance where to_tsvector('simple', instance.jsonb->>'title') @@ to_tsquery('simple','foo')");
   }
 
-  @Test(expected = QueryValidationException.class)
-  public void testCLIParseException() throws Exception {
-    testCLI("x=foo", null);
-  }
 }
